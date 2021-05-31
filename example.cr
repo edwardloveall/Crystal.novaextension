9a8610774742b2e8cda6afa55a4d1b5679d5111f
Edward Loveall <edward@edwardloveall.com>
2021-05-31 10:43:10 -0400

    Add an example.cr

---
+++ example.cr
@@ -0,0 +1,26 @@
+alias DogType = Labrador | Poodle | Dalmation
+
+class Dog < Species::Mammal
+  BARK = "Woof!"
+  WAG_COUNT = 3
+
+  getter name : String
+  getter type : DogType
+
+  def self.labrador(name : String)
+    new(type: Labrador, name: name)
+  end
+
+  def initialize(@type : DogType, @name : String)
+  end
+
+  def speak
+    BARK
+  end
+
+  def wag
+    WAG_COUNT.times do
+      puts "*wag's tail*"
+    end
+  end
+end
